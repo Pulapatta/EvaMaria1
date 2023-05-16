c7f31ffad3e799a6e1d7a898b4fd0bc85c139b3d
KuttiChathanTg <133741567+KuttiChathanTg@users.noreply.github.com>
2023-05-16 13:43:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-
 git clone -b master https://github.com/Pulapatta/EvaMaria1.git /EvaMaria1
-
 cd /EvaMaria1
-
 echo "Installing Requirements..."
+pip3 install -U -r requirements.txt
+echo "Starting Bot, Please Wait..."
+python3 bot.py
