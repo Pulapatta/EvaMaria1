fe31116dab7dda9e6de50a515f143ca9b0743265
Rafeeq <87922252+Pulapatta@users.noreply.github.com>
2023-05-16 08:25:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b EvaMaria1 https://github.com/Pulapatta/EvaMaria1.git /EvaMaria1
-cd /EvaMaria1
+
+git clone -b master https://github.com/josprojects/tgmoviebot.git /tgmoviebot
+
+cd /tgmoviebot
+
 echo "Installing Requirements..."
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot, Please Wait..."
+
 python3 bot.py
